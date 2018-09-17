ab8761406366dbb3b3a5209b6b3f678c54980473
David Kim <davidhonam@gmail.com>
2018-09-17 09:12:20 +0200

    refactor jquery

--- js/functions.js
+++ js/functions.js
@@ -1,7 +1,12 @@
-$(document).ready(function() {
-  $('.toggle-menu').on('click', function() {
-    $('.mobile-menu').slideToggle( "slow", function() {
-      $('.cover-section').toggleClass('nav-mobile--expanded');
-    })
-  })
+// Home Page
+var HomePage = HomePage || {};
+
+HomePage.toggleMobileMenu = function() {
+  $('.mobile-menu').slideToggle( "slow", function() {
+    $('.cover-section').toggleClass('nav-mobile--expanded');
+  });
+}
+
+$(function() {
+  $('.toggle-menu').on('click', HomePage.toggleMobileMenu);
 })
